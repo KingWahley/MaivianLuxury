4f22a9f14921697240e62c355d6ce92a8c1ab78f
KingWahley <kingwahley2@gmail.com>
2026-04-14 16:07:28 +0100

    updates

--- app/data/apartments.js
+++ app/data/apartments.js
@@ -51,7 +51,7 @@ export const apartments = [
       "Secured mini estate"
     ],
     images: Array.from({length: 12}, (_, i) => `/images/3 bedroom luxury apartment/${i + 1}.jpeg`),
-    img: "/images/3 bedroom luxury apartment/9.jpeg",
+    img: "/images/3 bedroom luxury apartment/1.jpeg",
     description: "Discover true comfort in this 3 bedroom luxury apartment. Handpicked for its unique charm, premium amenities, and unparalleled elegance. Make yourself at home in our curated collection of extraordinary residences."
   }
 ];
